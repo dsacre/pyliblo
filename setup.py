65eebe6b77074979527a3b8ee38d82d7fa3c5223
Dominic Sacré <dominic.sacre@gmx.de>
2007-02-19 22:36:16 +0000

    git-svn-id: svn+ssh://kobol/srv/svn/pyliblo@1 51631bcf-db29-0410-a538-d889976031e6

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup, Extension
+from Pyrex.Distutils import build_ext
+
+setup(
+    name = 'pyliblo',
+    version = '0.1',
+    author = 'Dominic Sacré',
+    author_email = 'dominic.sacre@gmx.de',
+    url = 'das.nasophon.de/pyliblo',
+    description = 'Python wrapper for the liblo OSC library',
+    license = "GPL",
+    ext_modules = [
+        Extension('liblo', ['liblo/liblo.pyx'], libraries = ['lo']),
+    ],
+    cmdclass = { 'build_ext': build_ext },
+    scripts = [
+        'scripts/send_osc',
+        'scripts/dump_osc'
+    ]
+)
