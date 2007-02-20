0c46cc9f5f13d051cf67799321f4d6164fe555c7
Dominic Sacré <dominic.sacre@gmx.de>
2007-02-20 00:50:34 +0000

    git-svn-id: svn+ssh://kobol/srv/svn/pyliblo@7 51631bcf-db29-0410-a538-d889976031e6

--- setup.py
+++ setup.py
@@ -2,27 +2,37 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup, Extension
-from Pyrex.Distutils import build_ext
+try:
+    from Pyrex.Distutils import build_ext
+except ImportError:
+    # no pyrex, build using existing .c file
+    kwargs = {
+        'ext_modules': [
+            Extension('liblo', ['liblo/liblo.c'], libraries = ['lo'])
+        ],
+    }
+else:
+    # build with pyrex
+    kwargs = {
+        'ext_modules': [
+            Extension('liblo', ['liblo/liblo.pyx'], libraries = ['lo'])
+        ],
+        'cmdclass': {
+            'build_ext': build_ext
+        },
+    }
 
 setup(
     name = 'pyliblo',
     version = '0.1',
     author = 'Dominic Sacré',
     author_email = 'dominic.sacre@gmx.de',
-    url = 'das.nasophon.de/pyliblo',
+    url = 'http://das.nasophon.de/pyliblo/',
     description = 'Python wrapper for the liblo OSC library',
     license = "GPL",
-    ext_modules = [
-        Extension(
-            'liblo',
-            ['liblo/liblo.pyx'],
-            libraries = ['lo']),
-    ],
-    cmdclass = {
-        'build_ext': build_ext,
-    },
     scripts = [
         'scripts/send_osc',
         'scripts/dump_osc',
-    ]
+    ],
+    **kwargs
 )
