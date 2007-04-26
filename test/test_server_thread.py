1a2651ffd3300bf372bfc2b1cb0cf8d3eb2362f6
Dominic Sacré <dominic.sacre@gmx.de>
2007-04-26 00:10:20 +0000

    git-svn-id: svn+ssh://kobol/srv/svn/pyliblo@24 51631bcf-db29-0410-a538-d889976031e6

--- test/test_server_thread.py
+++ test/test_server_thread.py
@@ -11,24 +11,35 @@ def foo_cb(path, args):
     for a in args:
         print "received argument %s of type %s" % (a, a.type)
 
-def bar_cb(path, args, src, user_data):
+def bar_cb(path, args, src):
     print "bar_cb():"
     print "message from", src.get_url()
     for a in args:
         print "received argument %s of type %s" % (a, a.type)
 
-def baz_cb(path, args):
-    print "baz_cb():"
-    print args[0]
+class Blah:
+    def __init__(self, x):
+        self.x = x
+    def baz_cb(self, path, args, src, user_data):
+        print "baz_cb():"
+        print args[0]
+        print "self.x is", self.x, ", user data was", user_data
 
 st.add_method('/foo', 'ifs', foo_cb)
 st.add_method('/bar', 'hdc', bar_cb)
-st.add_method('/baz', 'b',   baz_cb)
+
+b = Blah(123)
+st.add_method('/baz', 'b', b.baz_cb, 456)
 
 st.start()
 
 liblo.send(st.get_port(), "/foo", 123, 456.789, "buh!")
 liblo.send(st.get_port(), "/bar", 1234567890123456L, 666, ('c', "x"))
+
+time.sleep(1)
+st.stop()
+st.start()
+
 liblo.send(st.get_port(), "/baz", [1,2,3,4,5,6,7,8,9,10])
 
 time.sleep(1)
