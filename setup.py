81f2bc56b46bb94aa417dd02dac3501efea7112b
Dominic Sacré <dominic.sacre@gmx.de>
2007-12-14 22:36:20 +0000

    version 0.6.4
    git-svn-id: svn+ssh://kobol/srv/svn/pyliblo@56 51631bcf-db29-0410-a538-d889976031e6

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if with_pyrex:
 
 setup (
     name = 'pyliblo',
-    version = '0.6.3',
+    version = '0.6.4',
     author = 'Dominic Sacre',
     author_email = 'dominic.sacre@gmx.de',
     url = 'http://das.nasophon.de/pyliblo/',
